6543c12f190ddf8f3f70dce4ced2ed39b144b3b5
Haegonn <JsDalton11@gmail.com>
2015-08-28 11:07:56 -0400

    Initial Commit

---
+++ Count Words/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Introduce the program to the user.
+            Console.WriteLine("Welcome to Josh's Word Counter 1000!");
+
+            // Prompt the user to enter the string they would like counted.
+            Console.WriteLine("Please enter the string which you would like the words counted: ");
+            String originalString = Console.ReadLine();
+
+            // Set up the wordCount variable, which will be used to keep track of how many words we have.
+            int wordCount = 0;
+
+            // Loop over originalString and look for spaces.
+            foreach (char c in originalString)
+            {
+                if (c == ' ')
+                {
+                    wordCount++;
+                }
+            }
+
+            // Add 1 to wordCount to make up for the very last word.
+            wordCount += 1;
+
+            // Display the word count.
+            Console.WriteLine("Your word count is: {0}", wordCount);
+
+            // Pause the console so the user can see the output.
+            Console.WriteLine("Please press enter to exit the program. :)");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Count Words/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Count Words")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TEAM OS")]
+[assembly: AssemblyProduct("Count Words")]
+[assembly: AssemblyCopyright("Copyright © TEAM OS 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fe23185d-b52a-4747-8265-6c407bd5517d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
